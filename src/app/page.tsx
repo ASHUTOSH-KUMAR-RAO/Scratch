5aeafa50fe5889fa099cff6375b7aa69fc43f513
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2026-01-24 10:31:23 +0530

    06:background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const Page = async () => {
+const Page = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflow.queryOptions());
+  const queryClient = useQueryClient();
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient.invalidateQueries(trpc.getWorkflow.queryOptions());
+    }
+  }));
   return (
     <div className="min-h-screen flex items-center justify-center">
-  <h1>Hi</h1>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create Wordflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,24 @@
-import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
+import {  createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => {
+  getWorkflow: protectedProcedure.query(() => {
     // todo While the core principle holds: query → GET, mutations → POST—there are nuances
-    return prisma.user.findMany();
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async() => {
+
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "ashu@mail.com",
+      },
+    });
+    return prisma.workflow.create({
+      data: {
+        name: "New Workflow",
+      },
+    });
   }),
 });
 // export type definition of API
